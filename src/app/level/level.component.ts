62911d40abbf1f15bb86d013a745517c9ef12b1b
akonovalov <akonovalov@at-consulting.ru>
2017-04-22 14:05:42 +0300

    add param for level

--- src/app/level/level.component.ts
+++ src/app/level/level.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-level',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./level.component.scss']
 })
 export class LevelComponent implements OnInit {
+  level: number = 0;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.level = +this.route.snapshot.params['id'];
+  }
+
+  goToSlide(level) {
+    this.router.navigate(['../', level], { relativeTo: this.route })
   }
 
 }
