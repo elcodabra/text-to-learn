32522df1ac4a6cb438123c97571b23094169653c
aleksey <maylo.kondratev@yandex.ru>
2017-04-22 16:01:36 +0300

    Переверстал основной шаблон, сделал основную форму

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  @Input() ttl: string = 'Input your text';
+  
   constructor() { }
 
   ngOnInit() {
