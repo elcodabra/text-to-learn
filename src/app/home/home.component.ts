537ad5c9fe25876eee76f06af515995e6e51f8c4
aleksey <maylo.kondratev@yandex.ru>
2017-04-22 21:02:04 +0300

    Шаги, но еще надо поправить

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  @Input() ttl: string = 'Input your text';
+  private ttl: string = 'Input your text';
 
   constructor(private router: Router) { }
 
