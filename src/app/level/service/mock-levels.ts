54ab68cfe0dda745507c336d3ed6adaaf23dc342
akonovalov <akonovalov@at-consulting.ru>
2017-04-23 17:43:28 +0300

    change level names

--- src/app/level/service/mock-levels.ts
+++ src/app/level/service/mock-levels.ts
@@ -1,9 +1,9 @@
 import { Level } from './level.model';
 
 export const LEVELS: Level[] = [
-  {id: 0, name: 'View'},
-  {id: 1, name: 'Put sentences to the right order'},
-  {id: 2, name: 'Put words in sentences to the right order'},
-  {id: 3, name: 'Type the sentence by order'},
+  {id: 0, name: 'View your text'},
+  {id: 1, name: 'Put in the right order'},
+  {id: 2, name: 'Put in the right order'},
+  {id: 3, name: 'Type the sentence'},
   {id: 4, name: 'Type all text'}
 ];
