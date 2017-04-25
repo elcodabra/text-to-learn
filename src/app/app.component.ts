5fe3c9bb2ae6991022bd25146c686e39cd58b4bd
akonovalov <akonovalov@at-consulting.ru>
2017-04-25 09:12:30 +0300

    update welcome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'text-to-learn';
+
+  constructor(private location: Location) {
+    const path: string = location.path()
+  }
 }
